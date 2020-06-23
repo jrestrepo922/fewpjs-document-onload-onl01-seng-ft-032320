914adcfeb25d22b71b393a8a85611d702751f3a2
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-23 14:08:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Your code goes here
 // console.log("I did not wait till the DOM was loaded") This will always go first
-document.addEventListener("DOMContentLoaded", function() {console.log("The DOM has Loaded")});
+//document.addEventListener("DOMContentLoaded", function() {console.log("The DOM has Loaded")});
 
